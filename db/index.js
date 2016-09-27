932a4ea09036eee63edcc94087f56bfc95dd50b7
Brian Kilrain <bkilrain@gmail.com>
2016-09-26 17:13:33 -0700

    Connect DB to server

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+var config    = require(__dirname + '/config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
@@ -13,11 +13,10 @@ if (config.use_env_variable) {
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js') && (file !== "database.js");
   })
   .forEach(function(file) {
     var model = sequelize['import'](path.join(__dirname, file));
--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 
 const port = process.env.NODE_ENV === 'PROD' ? 80 : 3000;
+const db = require('../db/index.js').sequelize;
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 app.listen(port, function() {
   console.log('Listening on http://localhost:', port);
+   db.sync().then(function() {
+    console.log('Synced with PostgreSQL!');
+  });
 });
