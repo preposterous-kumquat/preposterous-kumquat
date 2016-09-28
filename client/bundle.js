a281451d8b28c4819cf975ae5c9b29db7ca18e3f
Josephine <bluejello@gmail.com>
2016-09-28 11:34:44 -0700

    Delete files

--- client/bundle.js
+++ client/bundle.js
@@ -28698,7 +28698,7 @@
 	      _react2.default.createElement(
 	        'span',
 	        null,
-	        '|'
+	        ' | '
 	      ),
 	      _react2.default.createElement(
 	        'span',
@@ -28712,7 +28712,7 @@
 	      _react2.default.createElement(
 	        'span',
 	        null,
-	        '|'
+	        ' | '
 	      ),
 	      _react2.default.createElement(
 	        'span',
@@ -28731,40 +28731,6 @@
 	    )
 	  );
 	};
-
-	// export default class MainLayout extends React.Component {
-	//   render() {
-	//     return (
-	// 			<div>
-	// 				<div className='navBar'>
-	//           <ul>
-	//             <li>
-	//               <Link to='/home'>HOME</Link>
-	//             </li>
-	//             <li>
-	//               <Link to='/upload'>UPLOAD PHOTO</Link>
-	//             </li>
-	//             <li>
-	//               <Link to='/signin'>SIGN OUT</Link>
-	//             </li>
-	//           </ul>
-	// 				</div>
-	// 			</div>
-	// 		);
-	//   }
-	// }
-
-	// ReactDOM.render((
-	//   <Router>
-	//     <Route path="/" component={MainLayout}>
-	//         <Route path="home" component={Home} />
-	//         <Route path="upload" component={Upload} />
-	//         <Route path="carousel" component={Carousel} />
-	//         <Route path="signup" component={SignUp} />
-	//         <Route path="signin" component={SignIn} />
-	//     </Route>
-	//   </Router>
-	// ), document.getElementById('app'));
 
 /***/ },
 /* 258 */
--- client/components/layouts/mainlayout.jsx
+++ client/components/layouts/mainlayout.jsx
@@ -7,11 +7,11 @@ export default props => (
       <span>
         <Link to='/home' activeClassName="active">HOME</Link>
       </span>
-      <span>|</span>
+      <span> | </span>
       <span>
         <Link to='/upload' activeClassName="active">UPLOAD PHOTO</Link>
       </span>
-      <span>|</span>
+      <span> | </span>
       <span>
         <Link to='/signin' activeClassName="active">SIGN OUT</Link>
       </span>
@@ -21,37 +21,3 @@ export default props => (
     </main>
   </div>
 );
-
-// export default class MainLayout extends React.Component {
-//   render() {
-//     return (
-// 			<div>
-// 				<div className='navBar'>
-//           <ul>
-//             <li>
-//               <Link to='/home'>HOME</Link>
-//             </li>
-//             <li>
-//               <Link to='/upload'>UPLOAD PHOTO</Link>
-//             </li>
-//             <li>
-//               <Link to='/signin'>SIGN OUT</Link>
-//             </li>
-//           </ul>
-// 				</div>
-// 			</div>
-// 		);
-//   }
-// }
-
-// ReactDOM.render((
-//   <Router>
-//     <Route path="/" component={MainLayout}>
-//         <Route path="home" component={Home} />
-//         <Route path="upload" component={Upload} />
-//         <Route path="carousel" component={Carousel} />
-//         <Route path="signup" component={SignUp} />
-//         <Route path="signin" component={SignIn} />
-//     </Route>
-//   </Router>
-// ), document.getElementById('app'));
