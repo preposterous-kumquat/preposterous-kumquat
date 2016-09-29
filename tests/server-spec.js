cba7e3cfd58c2c365b7fa919ded52e62f4dcb416
Gamerlazer <gamerlazer@gmail.com>
2016-09-29 15:07:03 -0700

    Brainstorm tests

---
+++ tests/server-spec.js
@@ -0,0 +1,28 @@
+/*
+
+1) Signup to return a session.user
+2) Expect all subsequent requests to have session object with user 
+2) Expect logging out to remove the session
+3) Expect non auth users to get a 401 with /upload
+4) Expect non auth users to get a 401 with /photos
+5) Expect non auth users to get a 401 with /stack 
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+// Expect logging out to remove the sessions
