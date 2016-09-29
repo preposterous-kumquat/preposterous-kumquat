c269146efd06b0cad316d905969d4214a90413cd
Gamerlazer <gamerlazer@gmail.com>
2016-09-28 21:25:35 -0700

    Pipe upload object to photo service and save in main db

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const ports = require('./config/config.js');
+const port = process.env.NODE_ENV === 'PROD' ? ports.PROD : ports.DEV;
 
-const port = process.env.NODE_ENV === 'PROD' ? 80 : 3000;
 const db = require('../db/index.js').sequelize;
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-app.listen(port, function() {
-  console.log('Listening on http://localhost:', port);
+app.listen(port.main, function() {
+  console.log('Listening on http://localhost:', port.main);
    db.sync().then(function() {
     console.log('Synced with PostgreSQL!');
   });
