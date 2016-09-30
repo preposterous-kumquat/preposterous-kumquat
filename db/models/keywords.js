e8c43dd01dba55471ba423ed63398c47236eabcd
Gamerlazer <gamerlazer@gmail.com>
2016-09-30 16:12:38 -0700

    Add Keyword and Theme join table

--- db/models/keywords.js
+++ db/models/keywords.js
@@ -6,6 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         Keywords.belongsToMany(models.Photos, {through: 'PhotosKeywords'});
+        Keywords.belongsToMany(models.Themes, {through: 'ThemeKeywords'});
       }
     }
   });
